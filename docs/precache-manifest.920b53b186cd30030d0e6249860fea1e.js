b5e77165fb393411ab5bf1ed974755f732b979e4
xeusteerapat <xeus085@gmail.com>
2021-02-07 15:31:43 +0700

    [ADD] test building files

---
+++ docs/precache-manifest.920b53b186cd30030d0e6249860fea1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4cd39d2804e30f5d680bb49ec354fa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2cb7cd06f926c2a8c91",
+    "url": "/static/css/main.08a634f1.chunk.css"
+  },
+  {
+    "revision": "178f349eecffa45d4f89",
+    "url": "/static/js/2.5d140ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d140ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cb7cd06f926c2a8c91",
+    "url": "/static/js/main.509e0257.chunk.js"
+  },
+  {
+    "revision": "5437ac1423d8ea888e2c",
+    "url": "/static/js/runtime-main.7eaa206f.js"
+  }
+]);
